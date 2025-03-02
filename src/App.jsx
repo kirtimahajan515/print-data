259cddc79732bd55a83de682d59f8c4924ac315c
kirti <mahajankirti515@gmail.com>
2025-03-02 14:45:47 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Body from "./components/Body";
+
+
+function App() {
+
+  const students = [
+    {rollNo:101, name:"Aarav Sharma"},
+    {rollNo:102, name:"Ishita Patil"},
+    {rollNo:103, name:"Roshan Gupta"},
+    {rollNo:104, name:"Neha Varma"},
+    {rollNo:105, name:"Kunal Mehta"},
+    {rollNo:106, name:"Simran Shah"},
+    {rollNo:107, name:"Aadity Rao"},
+    {rollNo:108, name:"Priya Iyer"},
+    {rollNo:109, name:"Varun Joshi"},
+    {rollNo:110, name:"Kalpesh Nakta"},
+  ]
+  return (
+  
+    students.map((student) => (
+         <Body key={student.id} name={student.name} rollNo={student.rollNo}/>
+    ))
+
+  )
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+
+
+function Body({rollNo, name}) {
+    
+return (
+     <div style={{padding:"1rem", margin:"1rem", backgroundColor:"gray"}}>
+        <p>RollNo : {rollNo}</p>
+        <p>Name : {name}</p>
+     </div>
+  )
+}
+
+export default Body
